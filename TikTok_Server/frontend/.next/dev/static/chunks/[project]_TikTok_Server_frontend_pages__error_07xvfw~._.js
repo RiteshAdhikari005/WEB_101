4d326b40a9c2976c1196b24409f88d98367f6790
Ritesh Adhikari <ritesh2005adhikari@gmail.com>
2026-05-14 22:04:49 +0530

    practicals

---
+++ TikTok_Server/frontend/.next/dev/static/chunks/[project]_TikTok_Server_frontend_pages__error_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0shl_next_dist_compiled_0y~49v-._.js","static/chunks/0shl_next_dist_shared_lib_0mkbju~._.js","static/chunks/0shl_next_dist_client_10orntp._.js","static/chunks/0shl_next_dist_08j22oz._.js","static/chunks/0vz._next_dist_pages__error_0zgt84s.js","static/chunks/[next]_entry_page-loader_ts_0pjpn-y._.js","static/chunks/0shl_react-dom_0upd-af._.js","static/chunks/0shl_0ic2.1z._.js","static/chunks/[root-of-the-server]__0~l.r2a._.js"],
+    source: "entry"
+});
